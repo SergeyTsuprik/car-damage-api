0d3a89a0f1dc3d2ce6aaa15af9f8afd04e7cc1f6
Sergey Ts <natalya.tsuprik@yandex.by>
2026-05-09 19:46:25 +0300

    Fix: восстановить импорт FileResponse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException, Header, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from ultralytics import YOLO
